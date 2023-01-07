f14e7dfba75ba80f9ba0a4bb670291fe338a6598
JGrund532 <122171805+JGrund532@users.noreply.github.com>
2023-01-07 19:02:40 +0000

    V1
    V1 with the function solver as a script ran by the main script

---
+++ BIN/Reworked.py
@@ -0,0 +1,52 @@
+# choose from 3 options
+
+print
+"hello world - please choose from the following via typing the number"
+
+optionsForPrint = "1 = cosh (ksdkahd), 2 = option 2 , 3 = option 3"
+loop
+options as option
+print
+option
+
+myOption = input(optionsForPrint)
+
+# input a number/value
+
+print
+"please type your value"
+input = myValue
+
+# do something
+
+myMathFuction = getOption(myOption);
+
+myArray = {"option1", "optio2"}  # list
+myMultiArray = {  # array
+    option1 = array("name, value"),
+option2 = array("name, value"),
+opton3 = hello
+}
+
+runMath = getattr(math, myMathFuction)
+result = runMath(myValue)
+
+function
+getOption(myOption)
+{
+
+    myOption = myOption - 1;
+mathsFunctions = array("cosh, option 2, option 3")
+
+return mathsFuctions[myOption];
+
+/ *loop(mathsFunctions as thisMathFunction) {
+if (thisMathFunction = myOption)
+{
+return = thisMathFunction
+}
+} * /
+
+return error
+
+}
---
+++ tools/fraction_to_float.py
@@ -0,0 +1,12 @@
+def convert_to_float(frac_str):
+    try:
+        return float(frac_str)
+    except ValueError:
+        num, denom = frac_str.split('/')
+        try:
+            leading, num = num.split(' ')
+            whole = float(leading)
+        except ValueError:
+            whole = 0
+        frac = float(num) / float(denom)
+        return whole - frac if whole < 0 else whole + frac
