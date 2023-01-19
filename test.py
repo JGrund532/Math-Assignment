59d007770c61552497a0cc46461675ad94a668d1
JGrund532 <122171805+JGrund532@users.noreply.github.com>
2023-01-19 19:00:54 +0000

    V8
    Probability binomial function fully working. full  expansion
    is displayed after every attempt of reevaluating r r

--- test.py
+++ test.py
@@ -10,14 +10,15 @@ attempt_count = 0
 class ProbabilityOperator (object):
     def __init__ (self):
 
+        #@classmethod
+        #def binom_calc(bpc):
         q, p, n = symbols('q p n')
-
 
         print("Pecentage of faulty parts: ")
         p = input ('p: ')
 
         print (f"{p}","%")
-        
+          
         p = float(p)/100
 
         q = 1 - p
@@ -26,29 +27,33 @@ class ProbabilityOperator (object):
         print("sample size: ")
         n = Integer(input('n: '))
 
+    #while loop to allow multiple attempts changing the value of r 
         while attempt_count < attempt_limit:
-            
+              
           print('Permutation value:')
           r = Integer(input('r: '))
 
           expansion = probability_formulas.formulas.binomial_expansion(q, p, n)
-          
+            
           expansion = str(expansion)
 
           print (expansion)
 
           expansion = expansion.split('+')
 
-          expansion = expansion[((int(r)*-1))-1]
+          expansion = expansion[((int(r)*-1)-1)]
 
           print (expansion, '=')
 
           expansion = eval(expansion)
 
           print(expansion)
-            
-          print("Reavlue r? Type N to cancel")
-
+          
+       #breaks while loop if user no longer needs       
+          print("Revalue r? Type N to cancel, Y to go again")
+          go_again = input('')
+          if go_again == 'N':
+            break
 
 
 ProbabilityOperator()
